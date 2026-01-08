e6577dfe4ade14d7f8eb71f40bf736bbf5d86f08
Wondersmile711 <grumsj2@gmail.com>
2026-01-08 11:23:40 +0100

    Fin du TP1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -13,12 +13,17 @@ public class Gaulois {
 		return nom;
 	}
 	
+	public void parler(String texte) {
+		System.out.println("Le gaulois " + nom + ": \"" + texte + "\".");
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + "envoie un grand coup dans la mâchoire de " + romain.getNom() + ".");
+		romain.recevoirCoup(force/3);
+	}
+	
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix", 8);
 		System.out.println(asterix.getNom());
-	}
-	
-	public void parler(String texte) {
-		System.out.println("Le gaulois " + nom + ": \"" + texte + "\".");
 	}
 }
--- src/test_fonctionnel/TestGaulois.java
+++ src/test_fonctionnel/TestGaulois.java
@@ -1,11 +1,21 @@
 package test_fonctionnel;
 
 import personnages.Gaulois;
+import personnages.Romain;
 
 public class TestGaulois {
 	public static void main(String[] args) {
 		Gaulois asterix = new Gaulois("Astérix", 8);
+		Romain minus = new Romain("Minus", 6);
+		
 		System.out.println(asterix.getNom());
 		asterix.parler("Bonjour");
+		minus.parler("UN GAU… UN GAUGAU…");
+		///for (int i = 0; i < 2; i ++) {
+			///minus.recevoirCoup(3);
+		///}
+		for (int i = 0; i < 3; i++) {
+			asterix.frapper(minus);
+		}
 	}
 }
